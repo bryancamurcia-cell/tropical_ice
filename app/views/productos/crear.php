589b41d94795dbacbaa2c0d378c58e5b4ddd2245
jssp05 <juansebastianparrapino8@gmail.com>
2026-09-24 17:42:48 -0500

    formulario para post

---
+++ app/views/productos/crear.php
@@ -0,0 +1,11 @@
+<form action ="/productos" method="POST">
+    
+    <input type="text" name="nombre" id="nombre" required><br>
+
+    
+    <input type="number" name="precio" id="precio" step="0.01" required><br>
+
+    <input type="number" name="stock" id="stock" required><br>
+
+    <button type="submit">guardar</button>
+</form>
